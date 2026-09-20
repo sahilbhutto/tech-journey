7bc833aafb6b6f5b95e26e29a06ba8d7992752d4
Sahil Ali Bhutto <sahil.bhutto.dev@gmail.com>
2026-09-20 22:05:40 +0500

    complete: error handling

--- ai_engineering/01_python/main.py
+++ ai_engineering/01_python/main.py
@@ -1,7 +1,38 @@
 
-file = open("data.txt", "w")
+def calculate(num1, num2, opt):
 
-file.write(" I am web developer.")
+    match opt:
+        case "+":
+            return num1 + num2
 
-file.close()
+        case "-":
+            return num1 - num2
 
+        case "*":
+            return num1 * num2
+
+        case "/":
+            return num1 / num2
+
+        case _:
+            raise ValueError("Invalid operation")
+
+
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    opt = input("Enter operation: +, -, *, /: ")
+
+    result = calculate(num1, num2, opt)
+
+except ValueError as error:
+    print(error)
+
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
+
+else:
+    print("\nResult:", result)
+
+finally:
+    print("=== Program End ===")
